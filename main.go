f0519b7cc654f65ee22b92a219b9883896e817e8
Dan Vulpe <dvulpe@gmail.com>
2020-07-29 21:31:54 +0100

    slow the responses down slightly

--- main.go
+++ main.go
@@ -120,6 +120,7 @@ func createHttpServer(successRate int) *http.Server {
 
 func Handler(successRate int) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Millisecond) // don't be too fast
 		if rand.Intn(101) > successRate {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(w, "Fail\n")
